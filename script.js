6a80025886f73cb1e1d1511a8ef362626638c997
laiza <laizacristina2507@gmail.com>
2026-02-19 16:45:55 -0300

    Novas alterações de javascript

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ const song = document.getElementById("audio");
 const start = document.getElementById("start");
 
 songName.innerText = "Sledgehammer";
+let isPlaying = false;
 
 function songStart(){
+    start.querySelector(".bi").classList.remove("bi-play-circle-fill");
+    start.querySelector(".bi").classList.add("bi-pause-circle-fill");
     song.play();
+    isPlaying = true;
 }
-start.addEventListener("click", songStart);
-song.pause()
+function pauseStart(){
+    start.querySelector(".bi").classList.add("bi-play-circle-fill");
+    start.querySelector(".bi").classList.remove("bi-pause-circle-fill");
+    song.pause();
+    isPlaying = false;
+}
+function playPauseDecider(){
+    if (isPlaying == true){
+        pauseStart()
+    }
+    else{
+        songStart();
+    }
+}
+start.addEventListener("click", playPauseDecider);
