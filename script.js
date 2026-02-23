ed2dbbf4b0894a3f575c4da0125747a6129cd507
laiza <laizacristina2507@gmail.com>
2026-02-23 15:21:39 -0300

    alterações de javacrispt renomeando as musicas

--- script.js
+++ script.js
@@ -3,10 +3,28 @@ const bandName = document.getElementById("band-name");
 const song = document.getElementById("audio");
 const cover = document.getElementById("cover");
 const start = document.getElementById("start");
+const next = document.getElementById("next");
+const previus = document.getElementById("previus");
 
+const sledGehammer = {
+    songName: "Sledgehammer",
+    artist: "Rihanna",
+    file: "Sledgehammer"
+};
+const depecheMode = {
+    songName: "Enjoy the Silence",
+    artist: "Depechemode",
+    file: "depeche"
+};
+const givenUp = {
+    songName: "Given Up",
+    artist: "Linkin Park",
+    file: "linkin"
+};
 songName.innerText = "Sledgehammer";
 let isPlaying = false;
-
+const playlist = [sledGehammer, depecheMode, givenUp];
+let index = 0;
 function songStart(){
     start.querySelector(".bi").classList.remove("bi-play-circle-fill");
     start.querySelector(".bi").classList.add("bi-pause-circle-fill");
@@ -27,4 +45,11 @@ function playPauseDecider(){
         songStart();
     }
 }
+function initializeSong(){
+    cover.src = `img/${playlist[index].file}.webp`;
+    song.src = `songs/${playlist[index].file}.mp3`;
+    songName.innerText=playlist[index].songName;
+    bandName.innerText=playlist[index].artist;
+}
+initializeSong();
 start.addEventListener("click", playPauseDecider);
